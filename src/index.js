274f92dd13ef36d3a6f0c09ba417fdfa9683ee16
Thangdaovan01 <pdaothu589@gmail.com>
2023-12-05 17:44:31 +0700

    commit05122023

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
-// app.use(express.static(path.join(__dirname, './resources/public')))
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
+app.use(express.urlencoded({
+    extended: true
+})); //middleware xu li form
+app.use(express.json());
 
 //Tempalte engine
 app.engine('hbs', handlebars.engine({
@@ -19,14 +22,23 @@ app.set('views', path.join(__dirname, 'resources\\views'));
 
 //console.log('PATH', path.join(__dirname, 'resources\\views'));
 app.get('/', (req, res) => {
-    //res.send("Hello World!");
     res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
-    //res.send("Hello World!");
     res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send("Hello World!");
+
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
